07ca5f00be8fca09cd2461447c5d0b28efd60099
Nicole Magerko <magerko3@illinois.edu>
2019-06-14 12:36:26 -0500

    Centered form, added button decoration, began logic for click pop-up window for descriptions on merch page

--- clean.js
+++ clean.js
@@ -1,9 +1,28 @@
 //FRONTEND
 
-var merch = document.querySelectorAll(".merch");
+var learnMoreButtons = document.querySelectorAll(".learnMore");
+var addToCart = document.getElementsByClassName("cart");
 
-for (var i = 0; i < merch.length; i++) {
-    merch[i].addEventListener("click", function () {
-        this.classList.add("fade");
+for (var i = 0; i < learnMoreButtons.length; i++) {
+    learnMoreButtons[i].addEventListener("click", function () {
+        console.log(this);
+
+
+        // var itemPictures = document.getElementsByClassName("merch");
+        // var url = itemPictures[i].getAttribute('src');
+
+        // itemPictures.forEach(function (picture) {
+        //     var url = picture.getAttribute('src');
+        // });
+
+        var width = 200 + "px";
+        var height = 200 + "px";
+        window.open(url, 'Image', 'width=width,height=height,resizable=1');
     });
 };
+
+// for (var i = 0; i < addToCart.length; i++) {
+//     addToCart[i].addEventListener("click", function () {
+//         alert("CLONKED");
+//     });
+// };
